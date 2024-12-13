b3b73e4ddea4b9a77d5d34ac61991774cbc582ee
Shaurya Tiwari <shauryatiwari04@gmail.com>
2024-12-13 21:20:59 +0530

    eventRoutes.js

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,35 +1,35 @@
 const express = require('express');
-const Event = require('../models/Event'); // Import the Event model
+const Event = require('../models/Event');
 
 const router = express.Router();
 
-// Get all events
+
 router.get('/', async (req, res) => {
   try {
-    const events = await Event.find(); // Retrieve all events from MongoDB
-    res.json(events); // Send the events as a JSON response
+    const events = await Event.find(); 
+    res.json(events); 
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-// Post new event
+
 router.post('/', async (req, res) => {
   const { title, description, eventDate, location } = req.body;
 
-  // Create a new event, eventDate is treated as a string
+  
   const event = new Event({
     title,
     description,
-    eventDate, // Keep eventDate as a string
+    eventDate,
     location
   });
 
   try {
-    const newEvent = await event.save(); // Save the event to MongoDB
-    res.status(201).json(newEvent); // Return the created event
+    const newEvent = await event.save(); 
+    res.status(201).json(newEvent);
   } catch (error) {
-    res.status(400).json({ message: error.message }); // Handle error during save
+    res.status(400).json({ message: error.message }); 
   }
 });
 
