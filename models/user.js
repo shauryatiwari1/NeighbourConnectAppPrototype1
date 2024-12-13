469bf5a66b3b09809d462e1f6ab242a4e0f251b4
Shaurya Tiwari <shauryatiwari04@gmail.com>
2024-12-13 21:23:19 +0530

    user.js

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   latitude: { type: Number, required: true },
   longitude: { type: Number, required: true },
-  // Add other fields as needed
+ 
 });
 
 module.exports = mongoose.model('User', userSchema);
